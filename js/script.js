0273e2fb8c8f623d297a9d1f7e8b9a988b6b100e
Łukasz Duszak <lukas94.webdeveloper@gmail.com>
2026-05-12 20:08:04 +0200

    added function welcomeMessage

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-console.log("Witam serdecznie wszystkich programistów! Ten kod jest już w repozytorium Git.");
-
-let changeColorButton = document.querySelector(".button__changeColorBackground");
+{
+   const welcomeMessage = () => {
+     console.log("Witam serdecznie wszystkich programistów! Ten kod jest już w repozytorium Git.");
+   }
+   welcomeMessage();
+const changeColorButton = document.querySelector(".button__changeColorBackground");
 
 changeColorButton = addEventListener("click", () => {
-    let body = document.querySelector(".body");
+    const body = document.querySelector(".body");
     body.classList.toogle("lightBackgroundColor");
 });
 
 changeColorButton();
+
+}
